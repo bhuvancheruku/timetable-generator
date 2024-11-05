a725868b705d2a1120eabaa15284e5fba2078f5a
maxedout_13 <34746125+bhuvancheruku@users.noreply.github.com>
2024-11-06 01:18:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -203,4 +203,4 @@ if st.button("Generate Timetable"):
                 st.download_button(
                     label="Download Timetable PDF",
                     data=pdf_buffer,
-                    file_name)
+                    file_name
